2365c3fb5cc9d64a272da9405c7ae0df43cb619c
M-Shams <mahdishams.13811381@gmail.com>
2023-01-20 02:26:36 +0330

    make connection

--- Linked/Controllers/HomeController.cs
+++ Linked/Controllers/HomeController.cs
@@ -9,6 +9,8 @@ using DataBase;
 using System.Threading.Tasks;
 using Domain;
 using System.Security.Claims;
+using Graph;
+using Microsoft.AspNetCore.Routing.Internal;
 
 namespace Linked.Controllers
 {
@@ -18,6 +20,7 @@ namespace Linked.Controllers
         private readonly UserRepo _User;
         public HomeController(ILogger<HomeController> logger , UserRepo User)
         {
+            
             _logger = logger;
             _User = User;
         }
@@ -34,6 +37,36 @@ namespace Linked.Controllers
             HomeCardViewModel homeCardViewModul = new HomeCardViewModel();
             homeCardViewModul.User = await _User.GetUserAsync(int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)));
             ViewData["img"] = homeCardViewModul.User.Profile;
+            //get the connectionof user(0 to5 )
+
+            //add to suggestion
+            List<User> ToAddSugg = new List<User>();
+            Graph.Graph g = new Graph.Graph();
+           foreach(User item1 in g.GetConnection(UserId))
+            {
+                ToAddSugg.Add(item1);
+                foreach (User item2 in GetConnection(item1.Id))
+                {
+                    ToAddSugg.Add(item2);
+                    foreach (User item3 in GetConnection(item2.Id))
+                    {
+                        ToAddSugg.Add(item3);
+                        foreach (User item4 in GetConnection(item2.Id))
+                        {
+                            ToAddSugg.Add(item4);
+                            foreach (User item5 in GetConnection(item4.Id))
+                            {
+                                ToAddSugg.Add(item5);
+                                foreach (User item6 in GetConnection(item5.Id))
+                                {
+                                    ToAddSugg.Add(item6);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            homeCardViewModul.Suggests = ToAddSugg;
             return View(homeCardViewModul);
         }
 
